2fea05aa90947c4dbcd3a0d4eb7df3be0f77f630
Mason Fox <masonfox1066@yahoo.com>
2021-05-02 03:53:44 -0400

    Contain functions for type fix

--- newtons_method/newtons_method.f90
+++ newtons_method/newtons_method.f90
@@ -3,7 +3,8 @@ implicit none
 
 !perform newton's method to find the root of a given function
 real :: initialGuess = 0, root, temp
-integer :: maxIter = 50, i = 1
+integer :: maxIter = 500, i = 1
+
 real, parameter :: tolerance = 0.00001, exactSoln = 4*atan(1.0)/6.0 !known ans to calculate error
 
 temp = initialGuess !root for prior iteration
@@ -20,24 +21,24 @@ do i = 1, maxIter
        temp = root
 end do
 
+contains
+
+        real function func(x) !calculates value of desired function at x
+        implicit none
+
+        real, intent(in) :: x
+        real, parameter :: pi = 4*atan(1.0)
+
+        func = tan(x-(pi/6.0)) 
+        end function
+
+
+        real function df(x) !calculates value of given derivative at x
+        implicit none
+
+        real, intent(in) :: x
+        real, parameter :: pi = 4*atan(1.0)
+
+        df = (1.0 / cos(x-(pi/6.0)))**2 !sec^2
+        end function
 end program
-
-
-real function func(x) !calculates value of desired function at x
-implicit none
-
-real, intent(in) :: x
-real, parameter :: pi = 4*atan(1.0)
-
-func = tan(x-(pi/6.0)) 
-end function
-
-
-real function df(x) !calculates value of given derivative at x
-implicit none
-
-real, intent(in) :: x
-real, parameter :: pi = 4*atan(1.0)
-
-df = (1.0 / cos(x-(pi/6.0)))**2 !sec^2
-end function
