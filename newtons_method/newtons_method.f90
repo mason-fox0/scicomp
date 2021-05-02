5363eeff2eb4d86340f6e9ce18af3d8a42242336
Mason Fox <masonfox1066@yahoo.com>
2021-05-02 04:40:48 -0400

    Fixed method error, improved precision

--- newtons_method/newtons_method.f90
+++ newtons_method/newtons_method.f90
@@ -2,15 +2,15 @@ program newtonsMethod
 implicit none
 
 !perform newton's method to find the root of a given function
-real :: initialGuess = 0, root, temp
-integer :: maxIter = 500, i = 1
-
-real, parameter :: tolerance = 0.00001, exactSoln = 4*atan(1.0)/6.0 !known ans to calculate error
+double precision :: initialGuess = 0, root, temp
+integer :: maxIter = 50, i
+real, parameter :: tolerance = 1E-12
+double precision, parameter :: exactSoln = 4*datan(1.0d0)/6.0 !known ans to calculate error
 
 temp = initialGuess !root for prior iteration
 
 do i = 1, maxIter
-        root = initialGuess - func(temp) / df(temp) !calculate new root
+        root = temp - func(temp) / df(temp) !calculate new root
         write(*,*) "Iteration: ", i, " Value: ", root, " Error: ", root - exactSoln
 
         if (abs(root - temp) < tolerance) then !convergence check
@@ -23,22 +23,22 @@ end do
 
 contains
 
-        real function func(x) !calculates value of desired function at x
+        double precision function func(x) !calculates value of desired function at x
         implicit none
 
-        real, intent(in) :: x
-        real, parameter :: pi = 4*atan(1.0)
+        double precision, intent(in) :: x
+        double precision, parameter :: pi = 4*datan(1.0d0)
 
-        func = tan(x-(pi/6.0)) 
+        func = dtan(x-(pi/6.0)) 
         end function
 
 
-        real function df(x) !calculates value of given derivative at x
+        double precision function df(x) !calculates value of given derivative at x
         implicit none
 
-        real, intent(in) :: x
-        real, parameter :: pi = 4*atan(1.0)
+        double precision, intent(in) :: x
+        double precision, parameter :: pi = 4*datan(1.0d0)
 
-        df = (1.0 / cos(x-(pi/6.0)))**2 !sec^2
+        df = (1.0 / dcos(x-(pi/6.0)))**2 !sec^2
         end function
 end program
