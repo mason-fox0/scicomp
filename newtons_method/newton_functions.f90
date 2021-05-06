56568a2306aa64350418174929bd6de16455de47
Mason Fox <masonfox1066@yahoo.com>
2021-05-06 10:17:49 -0400

    defined functions

--- newtons_method/newton_functions.f90
+++ newtons_method/newton_functions.f90
@@ -1,35 +1,44 @@
 module functions
+!provides function list for use with newtons_method
+use, intrinsic :: iso_fortran_env
+implicit none
+
+integer, parameter :: dp = real64
+real(dp) :: x
 
 contains
 
-double precision function f1(x)
+real(dp) function f1(x)
 implicit none
-
+f1 = cosh(x) - 5
 end function
 
-double precision function df1(x)
+real(dp) function df1(x)
 implicit none
-
+df1 = sinh(x)
 end function
 
-double precision function f2(x)
+real(dp) function f2(x)
 implicit none
-
+f2 = erf(x) !error function
 end function
 
-double precision function df2(x)
+real(dp) function df2(x)
 implicit none
+real(dp), parameter :: pi = 4*datan(1.0_dp)
+write(*,*) "Pi Precision Check: ", pi
 
+df2 = 2*exp(-x**2)/sqrt(pi)
 end function
 
-double precision function f3(x)
+real(dp) function f3(x)
 implicit none
-
+f3 = bessel_jn(0, x) !bessel func of first kind, order zero
 end function
 
-double precision function df3(x)
+real(dp) function df3(x)
 implicit none
-
+df3 = -bessel_jn(1, x) ! d(J_0(x))/dx = -J_1(x)
 end function
 
 end module
