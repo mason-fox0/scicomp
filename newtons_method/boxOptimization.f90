3c1739df911d65fa6a8a05d6c8ff3ae99fa60574
Mason Fox <masonfox1066@yahoo.com>
2021-05-07 10:33:30 -0400

    functions bug fix

--- newtons_method/boxOptimization.f90
+++ newtons_method/boxOptimization.f90
@@ -11,14 +11,14 @@ implicit none
 integer, parameter :: sp = real32, dp = real64
 real(dp) :: n = 100, m = 20
 real(dp) :: x
-real(dp) :: guess = 5
+real(dp) :: guess = 1.0
 integer :: iter, maxIter = 10
-real(dp) :: tolerance = 1E-12
+real(dp) :: tolerance = 1E-8
 
 call findRoot(dV, d2V, guess, iter, x, maxIter, tolerance) !critical point occurs where dV = 0
-if (x > 0 .and. x < 2*m .and. x < 2*n) then
+if (x > 0 .and. x < m/2.0 .and. x < n/2.0) then
         write(*,*) "Number of Iterations: ", iter
-        write(*,*) "Critical point: ", x
+        write(*,*) "Best Side Length: ", x
         write(*,*) "Max volume: ", volume(x)
 else 
         write(*,*) "Method Failed. Try a different guess." !todo: make more robust (min vs max)
@@ -33,12 +33,12 @@ contains
 
         real(dp) function dV(x)
                 real(dp), intent(in) :: x
-                dV = 4*x**3 - (n+m) * 2*x**2 + n*m * x 
+                dV = 12*x**2 - 4*(n+m) * x + n*m
         end function
 
         real(dp) function d2V(x)
                 real(dp), intent(in) :: x
-                d2V = 12*x**2 - 4*(n+m) * x + (n*m)
+                d2V = 24*x - 4*(n+m)
         end function
 
 end program
